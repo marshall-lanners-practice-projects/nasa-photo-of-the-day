0dfc7174bd54b8d17a5a9a71fd8a4c9616dbae9a
Edwin Chajon <junchajo@gmail.com>
2019-10-14 18:40:10 -0700

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
+import NasaList from "./Components/NasaList";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        <NasaList/>
       </p>
     </div>
   );
---
+++ src/Components/NasaCard.js
@@ -0,0 +1,27 @@
+
+import React from "react";
+import { Card, CardText, CardBody, CardTitle, CardSubtitle, Col } from "reactstrap";
+const NasaCard = props => {
+  return (
+    console.log(props)
+    <div></div>
+  );
+};
+export default NasaCard;
+
+
+
+
+
+
+/*<Col xs="12" sm="8">
+      <Card>
+      <img top width="100%" src={props.image} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Image title: {props.title}</CardTitle>
+          <CardText>{props.explanation}</CardText>
+          <CardSubtitle>Copyright: {props.copyright}</CardSubtitle>
+          <CardSubtitle>Date: <small className="text-warning">{props.date}</small></CardSubtitle>
+        </CardBody>
+      </Card>
+    </Col>*/
